2adc0ad025a849f8da311267a782993be14bdf5b
Даниил <@DESKTOP-5F695V5>
2026-09-14 11:36:52 +0500

    Task2

---
+++ Tyuiu.AndreevDE.Sprint0.Task2.V0.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.AndreevDE.Sprint0.Task2.V0.lib;
+namespace Tyuiu.AndreevDE.Sprint0.Task2.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckGetMessageVakid()
+        {
+            var name = "Даниил";
+            var res = DataService.GetMessage(name);
+            Assert.AreEqual("Привет, Даниил", res);
+        }
+    }
+}
---
+++ Tyuiu.AndreevDE.Sprint0.Task2.V0/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.AndreevDE.Sprint0.Task2.V0.lib;
+namespace Tyuiu.AndreevDE.Sprint0.Task2.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DataService.GetMessage("Даниил"));
+            Console.ReadKey();
+        }
+    }
+}
